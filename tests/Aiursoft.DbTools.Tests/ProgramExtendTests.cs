59a9a0ea742b997bf6b9fc1d401afef960194e51
Anduin Xue <anduin@aiursoft.com>
2023-06-28 15:43:29 +0000

    Restore a test.

---
+++ tests/Aiursoft.DbTools.Tests/ProgramExtendTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aiursoft.DbTools.Tests
+{
+    [TestClass]
+    public class ProgramExtendsTests
+    {
+        [TestMethod]
+        public async Task UpdateDbAsync_ShouldNotMigrate_WhenInEntityFramework()
+        {
+            // Arrange
+            var hostBuilder = Host.CreateDefaultBuilder();
+            hostBuilder.ConfigureServices(services => 
+                services.AddDbContext<MyDbContext>(config => 
+                    config.UseSqlite(@"DataSource=app.db;Cache=Shared"))
+            );
+            var host = hostBuilder.Build();
+            _ = await host.UpdateDbAsync<MyDbContext>();
+        }
+    }
+}
