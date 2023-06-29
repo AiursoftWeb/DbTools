1c30f4d92ed6c10096216126f64b7a82913879b4
Anduin Xue <anduin@aiursoft.com>
2023-06-29 11:39:40 +0000

    6.0.25. Add RegisterExtensions.cs. Add basic performance test.

--- src/Aiursoft.DbTools/RegisterExtensions.cs
+++ src/Aiursoft.DbTools/RegisterExtensions.cs
@@ -1,7 +1,5 @@
-﻿using Aiursoft.CSTools.Tools;
-using EFCoreSecondLevelCacheInterceptor;
+﻿using EFCoreSecondLevelCacheInterceptor;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Aiursoft.DbTools;
@@ -9,81 +7,46 @@ namespace Aiursoft.DbTools;
 public static class RegisterExtensions
 {
     public static IServiceCollection AddSqlServerDbContextWithCache<T>(
-        this IServiceCollection services, 
+        this IServiceCollection services,
         string connectionString,
         bool allowCache = true)
         where T : DbContext
     {
-        return services.AddDbContextPool<T>((serviceProvider, optionsBuilder) =>
-        {
+        services.AddDbContextPool<T>((serviceProvider, optionsBuilder) =>
             optionsBuilder
-                .UseSqlServer(
-                    connectionString: connectionString,
-                    sqlServerOptionsAction: options =>
-                    {
-                        options.CommandTimeout(30);
-                        options.EnableRetryOnFailure();
-                    });
-
-            if (allowCache)
-            {
-                // Configure the DbContext to use memory cache
-                var memoryCache = serviceProvider.GetRequiredService<IMemoryCache>();
-                optionsBuilder.UseMemoryCache(memoryCache);
-            }
-        });
-    }
-    
-    public static IServiceCollection AddSqliteDbContextWithCache<T>(
-        this IServiceCollection services, 
-        string connectionString,
-        bool allowCache = true)
-        where T : DbContext
-    {
-        return services.AddDbContextPool<T>((serviceProvider, optionsBuilder) =>
-        {
-            optionsBuilder
-                .UseSqlite(
-                    connectionString: connectionString,
-                    sqliteOptionsAction: options =>
-                    {
-                        options.CommandTimeout(30);
-                    });
-
-            if (allowCache)
-            {
-                // Configure the DbContext to use memory cache
-                var memoryCache = serviceProvider.GetRequiredService<IMemoryCache>();
-                optionsBuilder.UseMemoryCache(memoryCache);
-            }
-        });
-    }
-
-    // TODO: Benchmark the performance and find best practice.
-    public static IServiceCollection AddDbContextWithCacheThirdParty<T>(this IServiceCollection services, string connectionString)
-        where T : DbContext
-    {
-        if (EntryExtends.IsInUnitTests())
-        {
-            services.AddDbContext<T>((serviceProvider, optionsBuilder) =>
-                optionsBuilder
-                    .UseInMemoryDatabase("inmemory")
-                    .AddInterceptors(serviceProvider.GetRequiredService<SecondLevelCacheInterceptor>()));
-        }
-        // Consider some new EF technology like use memory cache.
-        else
-        {
-            services.AddDbContextPool<T>((serviceProvider, optionsBuilder) =>
-                optionsBuilder
-                    .UseSqlServer(connectionString)
-                    .AddInterceptors(serviceProvider.GetRequiredService<SecondLevelCacheInterceptor>()));
-        }
+                .UseSqlServer(connectionString)
+                .AddInterceptors(serviceProvider.GetRequiredService<SecondLevelCacheInterceptor>()));
 
         services.AddEFSecondLevelCache(options =>
         {
             options.UseMemoryCacheProvider().DisableLogging(true);
-            options.CacheAllQueries(CacheExpirationMode.Absolute, TimeSpan.FromMinutes(30));
+            options.CacheAllQueries(CacheExpirationMode.Sliding, TimeSpan.FromMinutes(30));
         });
         return services;
+    }
+
+    public static IServiceCollection AddSqliteDbContext<T>(
+        this IServiceCollection services,
+        string connectionString)
+        where T : DbContext
+    {
+        return services.AddDbContext<T>(optionsBuilder =>
+        {
+            optionsBuilder
+                .UseSqlite(
+                    connectionString: connectionString,
+                    sqliteOptionsAction: options => { options.CommandTimeout(30); });
+        });
+    }
+
+    public static IServiceCollection AddInMemoryDb<T>(
+        this IServiceCollection services)
+        where T : DbContext
+    {
+        return services.AddDbContext<T>(optionsBuilder =>
+        {
+            optionsBuilder
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString());
+        });
     }
 }
---
+++ tests/Aiursoft.DbTools.Tests/PerformanceTest.cs
@@ -0,0 +1,151 @@
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aiursoft.DbTools.Tests;
+
+public class PerformanceTestDb : DbContext
+{
+    public PerformanceTestDb(DbContextOptions<PerformanceTestDb> options) : base(options)
+    {
+    }
+
+    public DbSet<Book> Books => Set<Book>();
+}
+
+[TestClass]
+public class PerformanceTest
+{
+    private const string Sqlite = "DataSource=app.db;Cache=Shared";
+    
+    [TestMethod]
+    public async Task TestSqliteDefaultPool()
+    {
+        var services = new ServiceCollection();
+        services.AddLogging();
+        services.AddDbContextPool<PerformanceTestDb>(optionsBuilder =>
+            optionsBuilder.UseSqlite(Sqlite));
+        var built = services.BuildServiceProvider();
+        var db = built.GetRequiredService<PerformanceTestDb>();
+        await TestDb(db);
+    }
+    
+    [TestMethod]
+    public async Task TestSqliteDefaultContext()
+    {
+        var services = new ServiceCollection();
+        services.AddLogging();
+        services.AddDbContext<PerformanceTestDb>(optionsBuilder =>
+            optionsBuilder.UseSqlite(Sqlite));
+        var built = services.BuildServiceProvider();
+        var db = built.GetRequiredService<PerformanceTestDb>();
+        await TestDb(db);
+    }
+    
+    [TestMethod]
+    public async Task TestMemoryDefaultContext()
+    {
+        var services = new ServiceCollection();
+        services.AddLogging();
+        services.AddInMemoryDb<PerformanceTestDb>();
+        var built = services.BuildServiceProvider();
+        var db = built.GetRequiredService<PerformanceTestDb>();
+        await TestDb(db);
+    }
+
+    private async Task TestDb(PerformanceTestDb db)
+    {
+        var totalWatch = new Stopwatch();
+        totalWatch.Start();
+
+        await RunWithWatch(async () =>
+        {
+            try
+            {
+                await db.Database.EnsureDeletedAsync();
+                await db.Database.EnsureCreatedAsync();
+                await db.Database.MigrateAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }, "Database created!");
+        
+        await RunWithWatch(async () =>
+        {
+            for (int i = 0; i < 3000; i++)
+            {
+                db.Books.Add(new Book
+                {
+                    Name = Guid.NewGuid().ToString()
+                });
+                await db.SaveChangesAsync();
+            }
+        }, "Database inserted 3000 items!");
+
+        await RunWithWatch(async () =>
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                var results = await db.Books.ToListAsync();
+                _ = results.Count;
+            }
+        }, "Database tolisted 10000 times!");
+
+        await RunWithWatch(async () =>
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                var results = await db.Books.Where(b => b.Name.Contains(i.ToString())).ToListAsync();
+                _ = results.Count;
+            }
+        }, "Database queried with where condition 1000 times!");
+        
+        await RunWithWatch(async () =>
+        {
+            for (int i = 0; i < 700; i++)
+            {
+                // Insert.
+                db.Books.Add(new Book
+                {
+                    Name = Guid.NewGuid().ToString()
+                });
+                await db.SaveChangesAsync();
+                
+                // Then query
+                var results = await db.Books.Where(b => b.Name.Contains(i.ToString())).ToListAsync();
+                _ = results.Count;
+            }
+        }, "Database inserted-then-queried with condition 700 times!");
+        
+        await RunWithWatch(async () =>
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                // Insert.
+                db.Books.Remove(await db.Books.FirstAsync());
+                await db.SaveChangesAsync();
+                
+                // Then query
+                var results = await db.Books.Where(b => b.Name.Contains(i.ToString())).ToListAsync();
+                _ = results.Count;
+            }
+        }, "Database delete-then-queried with condition 500 times!");
+
+        totalWatch.Stop();
+        Console.WriteLine(totalWatch.Elapsed + " Totally elapsed!");
+        
+        await db.Database.EnsureDeletedAsync();
+    }
+
+    private async Task RunWithWatch(Func<Task> taskFactory, string whatDone)
+    {
+        var watch = new Stopwatch();
+        watch.Start();
+        await taskFactory();
+        watch.Stop();
+        Console.WriteLine(watch.Elapsed + " " + whatDone);
+    }
+}
